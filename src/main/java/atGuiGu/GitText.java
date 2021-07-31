2f1715b297fda2177636708401718c779197df36
xingzhimeng <@qq2079556893>
2021-07-31 17:16:54 +0800

    first commit

---
+++ src/main/java/atGuiGu/GitText.java
@@ -0,0 +1,14 @@
+package atGuiGu;
+
+/**
+ * @eauthor shkstart
+ * @ecreate }-07-31
+ */
+public class GitText {
+    public static void main(String[] args) {
+
+        System.out.println("hello git");
+
+
+    }
+}
